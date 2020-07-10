ca426d5774b40ca00c1edcbf31c86c3d7453b263
AmadorZcv <lucasamadorzcv@gmail.com>
2020-07-10 08:11:15 -0300

    :heavy_plus_sign: Adding CSV parser

--- mix.exs
+++ mix.exs
@@ -44,7 +44,8 @@ defmodule DraftTheory.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:csv, "~> 2.3"}
     ]
   end
 
